643da728e3b24b342cb25b28380753afdfa79891
debbycreamy <oliverdeborahchidiomimi@gmail.com>
2025-12-02 11:25:24 +0100

    Install @tailwindcss/vite and update lockfile

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
-// tailwind.config.js
-export default {
-  // or module.exports = { ... } if using CommonJS
-  darkMode: "class", // This tells Tailwind to use the 'dark' class on an ancestor element (like html)
-  content: ["./index.html", "./src/**/*.{js,jsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
+// // tailwind.config.js
+// export default {
+//   // or module.exports = { ... } if using CommonJS
+//   darkMode: "class", // This tells Tailwind to use the 'dark' class on an ancestor element (like html)
+//   content: ["./index.html", "./src/**/*.{js,jsx}"],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// };
